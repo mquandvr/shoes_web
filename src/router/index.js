b5bf2771f67406686f5bc89db834fdce32089abb
M.Quan <mquan2793@gmail.com>
2018-08-01 20:13:13 +0700

    chinh sua router

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,10 @@ const Page500 = () => import('@/views/pages/Page500')
 const Login = () => import('@/views/pages/Login')
 const Register = () => import('@/views/pages/Register')
 
+// Users
+const Users = () => import('@/views/users/Users')
+const User = () => import('@/views/users/User')
+
 // Employees
 const Employees = () => import('@/views/people/Employees')
 const EmployeesAdd = () => import('@/views/people/EmployeesAdd')
@@ -78,27 +82,55 @@ export default new Router({
             }
           ]
         },
-      ]
-    },
-    {
-      path: '404',
-      name: 'Page404',
-      component: Page404
-    },
-    {
-      path: '500',
-      name: 'Page500',
-      component: Page500
-    },
-    {
-      path: 'login',
-      name: 'Login',
-      component: Login
-    },
-    {
-      path: 'register',
-      name: 'Register',
-      component: Register
-    }
-  ]
+        {
+          path: 'users',
+          meta: { label: 'Users'},
+          component: {
+            render (c) { return c('router-view') }
+          },
+          children: [
+            {
+              path: '',
+              component: Users,
+            },
+            {
+              path: ':id',
+              meta: { label: 'User Details'},
+              name: 'User',
+              component: User,
+            },
+          ]
+        },
+      {
+        path: '/pages',
+        redirect: '/pages/404',
+        name: 'Pages',
+        component: {
+          render (c) { return c('router-view') }
+        },
+        children: [
+          {
+            path: '404',
+            name: 'Page404',
+            component: Page404
+          },
+          {
+            path: '500',
+            name: 'Page500',
+            component: Page500
+          },
+          {
+            path: 'login',
+            name: 'Login',
+            component: Login
+          },
+          {
+            path: 'register',
+            name: 'Register',
+            component: Register
+          }
+        ]
+      }
+    ]
+  }]
 })
