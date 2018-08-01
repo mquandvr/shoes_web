a5f008f37c393d8dc3a89dd590665795dda5cc5a
M.Quan <mquan2793@gmail.com>
2018-08-01 23:50:21 +0700

    them man hinh nha cung cap, cua hang

---
+++ src/views/admin/providers/ProviderData.js
@@ -0,0 +1,32 @@
+const providerData = [
+    {id: 0, name: 'John Doe', address: '12 SanFran..', category: 'May mặc'},
+    {id: 1, name: 'Samppa Nori', address: '12 SanFran..', category: 'May mặc'},
+    {id: 2, name: 'Estavan Lykos', address: '12 SanFran..', category: 'May mặc'},
+    {id: 3, name: 'Chetan Mohamed', address: '12 SanFran..', category: 'May mặc'},
+    {id: 4, name: 'Derick Maximinus', address: '12 SanFran..', category: 'May mặc'},
+    {id: 5, name: 'Friderik Dávid', address: '12 SanFran..', category: 'May mặc'},
+    {id: 6, name: 'Yiorgos Avraamu', address: '12 SanFran..', category: 'May mặc'},
+    {id: 7, name: 'Avram Tarasios', address: '12 SanFran..', category: 'May mặc'},
+    {id: 8, name: 'Quintin Ed', address: '12 SanFran..', category: 'May mặc'},
+    {id: 9, name: 'Enéas Kwadwo', address: '12 SanFran..', category: 'May mặc'},
+    {id: 10, name: 'Agapetus Tadeáš', address: '12 SanFran..', category: 'May mặc'},
+    {id: 11, name: 'Carwyn Fachtna', address: '12 SanFran..', category: 'May mặc'},
+    {id: 12, name: 'Nehemiah Tatius', address: '12 SanFran..', category: 'May mặc'},
+    {id: 13, name: 'Ebbe Gemariah', address: '12 SanFran..', category: 'May mặc'},
+    {id: 14, name: 'Eustorgios Amulius', address: '12 SanFran..', category: 'May mặc'},
+    {id: 15, name: 'Leopold Gáspár', address: '12 SanFran..', category: 'May mặc'},
+    {id: 16, name: 'Pompeius René', address: '12 SanFran..', category: 'May mặc'},
+    {id: 17, name: 'Paĉjo Jadon', address: '12 SanFran..', category: 'May mặc'},
+    {id: 18, name: 'Micheal Mercurius', address: '12 SanFran..', category: 'May mặc'},
+    {id: 19, name: 'Ganesha Dubhghall', address: '12 SanFran..', category: 'May mặc'},
+    {id: 20, name: 'Hiroto Šimun', address: '12 SanFran..', category: 'May mặc'},
+    {id: 21, name: 'Vishnu Serghei', address: '12 SanFran..', category: 'May mặc'},
+    {id: 22, name: 'Zbyněk Phoibos', address: '12 SanFran..', category: 'May mặc'},
+    {id: 23, name: 'Einar Randall', address: '12 SanFran..', category: 'May mặc'},
+    {id: 24, name: 'Félix Troels', address: '12 SanFran..', category: 'May mặc'},
+    {id: 25, name: 'Aulus Agmundr', address: '12 SanFran..', category: 'May mặc'},
+    {id: 42, name: 'Ford Prefex', address: '12 SanFran..', category: 'May mặc'}
+  ]
+  
+  export default providerData
+  
