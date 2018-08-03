e0aa3b92081f742967e2d0189f11967aae045426
M.Quan <mquan2793@gmail.com>
2018-08-03 23:51:36 +0700

    chinh sua common goi api

--- src/api/http-common.js
+++ src/api/http-common.js
@@ -1,10 +1,32 @@
 import axios from 'axios'
-//import cons from '@/api/http-common'
+import { HTTP_REQUEST_URL, HTTP_ACCESS_TOKEN } from '@/utils/constants'
+import { AUTH_LOGOUT } from '@/store/modules/auth/auth-mutation-types'
 
-export const HTTP = axios.create({
-  baseURL: 'https://www.yesno.wtf/api/',
-  headers: {
-    Authorization: 'Bearer {token}'
-  }
+const http = axios.create({
+  baseURL: HTTP_REQUEST_URL
 })
 
+http.interceptors.response.use(undefined, function (err) {
+  return new Promise(function (resolve, reject) {
+    if (err.status === 401 && err.config && !err.config.__isRetryRequest) {
+    // if you ever get an unauthorized, logout the user
+      this.$store.dispatch(AUTH_LOGOUT)
+    // you can also redirect to /login if needed !
+    }
+    Promise.reject(reject)
+    throw err;
+  });
+});
+
+// Set the AUTH token for any request
+http.interceptors.request.use(function (config) {
+  const token = localStorage.getItem(HTTP_ACCESS_TOKEN);
+  if (token) {
+    http.defaults.headers.common['Authorization'] = token;
+  } else {
+    delete http.defaults.headers.common['Authorization'];
+  }
+  return config;
+});
+
+export default http
--- src/router/index.js
+++ src/router/index.js
@@ -239,7 +239,7 @@ router.beforeEach((to, from, next) => {
   if (to.matched.some(record => record.meta.requiresAuth)) {
     // this route requires auth, check if logged in
     // if not, redirect to login page.
-    if (!store.getters.isAuthenticated) {
+    if (!store.getters.isAuthenticated && localStorage.getItem("access-token") === null) {
       next({
         name: 'Login',
         query: { redirect: to.fullPath }
